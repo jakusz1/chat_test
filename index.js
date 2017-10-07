87d63315401705c6df8c1ec966a3678ee43a283c
jakusz1 <13085272+jakusz1@users.noreply.github.com>
2017-10-07 21:36:09 +0200

    add nicknames

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('chat message', function(msg,user){
+    io.emit('chat message', msg, user);
   });
 });
 
